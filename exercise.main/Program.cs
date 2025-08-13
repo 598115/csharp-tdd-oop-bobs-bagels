349e60346e80e284fccf07d6e4fb26c123ac84ce
Trym Haugan Berger <trym.haugan.berger@experisacademy.no>
2025-08-13 08:42:24 +0200

    error fix

--- exercise.main/Program.cs
+++ exercise.main/Program.cs
@@ -7,15 +7,9 @@ Bagel PlainBaconBagel = new(new Filling(FillingType.Bacon) ,BagelType.Plain);
 Bagel OnionHamBagel = new(new Filling(FillingType.Ham), BagelType.Onion);
 Coffee cappucino = new(CoffeeType.Capuccino);
 
-
 ProductBundle bundle = new(BundleType.coffeeBagel);
 bundle.CoffeeAndBagelDeal(new Filling(FillingType.Bacon), BagelType.Onion);
-
-
-
 ProductBundle bundle2 = new(BundleType.bagel6);
-
-
 
 Filling filling = new Filling(FillingType.Ham);
 bundle2.BagelDeal6(BagelType.Sesame, filling, filling, filling, filling, filling, filling);
@@ -25,8 +19,4 @@ basket.AddItem(OnionHamBagel);
 basket.AddItem(bundle2);
 basket.AddItem(cappucino);
 basket.AddItem(bundle);
-
 basket.PrintReceipt();
-
-
-basket.
